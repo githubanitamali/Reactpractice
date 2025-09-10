3545e364b043a1f9b69553288224aa626094414f
DESKTOP-RBEAE3M\ashis <anitakorde001@gmail.com>
2025-09-10 11:20:30 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement("h1", {id : "heading"}, "Hello world by react")
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(heading)
+
+const parent = React.createElement(
+    "div", 
+    { id : "parent" }, 
+    React.createElement("div", { id : "child" },[
+        React.createElement("h1", { id : "heading" },"I am h1 tag"), 
+        React.createElement("h2", { id : "heading2" },"I am h2 tag"),
+    ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
